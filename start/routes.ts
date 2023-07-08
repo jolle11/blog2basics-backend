7e90dc89decfeb086177b33f8d74ad829f3bec98
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2023-07-08 19:59:44 +0200

    Auth and Blogs routes

--- start/routes.ts
+++ start/routes.ts
@@ -1,25 +1,16 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes.ts` as follows
-|
-| import './routes/cart'
-| import './routes/customer'
-|
-*/
+import Route from '@ioc:Adonis/Core/Route';
 
-import Route from '@ioc:Adonis/Core/Route'
+Route.group(() => {
+	Route.post('register', 'AuthController.register');
+	Route.post('login', 'AuthController.login');
 
-Route.get('/', async () => {
-  return { hello: 'world' }
-})
+	Route.group(() => {
+		Route.get('/', 'BlogController.list');
+		Route.get('/:id', 'BlogController.get');
+		Route.post('/new', 'BlogController.store');
+		Route.patch('/update', 'BlogController.edit');
+		Route.delete('/:id', 'BlogController.delete');
+	})
+		.prefix('blogs')
+		.middleware('auth:api');
+}).prefix('api');
