c31eca85738f280e9a91feba7e54a359fa7cf6df
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2023-07-10 13:15:35 +0200

    WIP - get, store & delete

--- app/Controllers/Http/BlogController.ts
+++ app/Controllers/Http/BlogController.ts
@@ -4,36 +4,47 @@ import Blog from 'App/Models/Blog';
 export default class BlogController {
 	public async list() {
 		const blogs = await Blog.all();
-
 		return blogs.reverse();
 	}
 
 	public async get({ params }: HttpContextContract) {
-		try {
-			const userBlog = await Blog.findBy('id', params.id);
-			return userBlog;
-		} catch (error) {
-			return error;
+		const blog = await Blog.findByOrFail('id', params.id);
+		return blog;
+	}
+
+	public async store({ request, response, auth }: HttpContextContract) {
+		const user = await auth.authenticate();
+		const data = request.all();
+		const userHasBlog = Blog.findBy('user_id', auth.user?.id);
+
+		if (Object.keys(userHasBlog).length === 0 && user) {
+			const blog = new Blog();
+
+			blog.name = data.name;
+			blog.description = data.description;
+
+			await user.related('blog').save(blog);
+
+			return blog;
+		} else {
+			return response
+				.status(403)
+				.send({ status: '403 Forbidden', message: 'Only one blog per user' });
 		}
 	}
 
-	public async store({ request, auth }: HttpContextContract) {
-		try {
-			const data = request.all();
+	// public async edit({ params, request, response, auth }: HttpContextContract) {}
 
-			if (auth.isLoggedIn) {
-				const user = await auth.authenticate();
-				const blog = new Blog();
+	public async delete({ params, auth, response }: HttpContextContract) {
+		const user = await auth.authenticate();
+		const blog = await Blog.findBy('id', params.id);
 
-				blog.name = data.name;
-				blog.description = data.description;
+		if (user && blog) {
+			await blog.delete();
 
-				await user.related('blog').save(blog);
-
-				return blog;
-			}
-		} catch (error) {
-			return error;
+			return response
+				.status(200)
+				.send({ status: '200 OK', message: 'Blog deleted' });
 		}
 	}
 }
