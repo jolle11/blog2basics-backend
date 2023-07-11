2f8969c5c196094391766d5f2054a4fe213bb3ab
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2023-07-11 11:32:59 +0200

    Added posts routes and refactored for guest blog and post availability

--- start/routes.ts
+++ start/routes.ts
@@ -1,16 +1,27 @@
 import Route from '@ioc:Adonis/Core/Route';
 
 Route.group(() => {
+	// ======================== AUTH ======================== //
 	Route.post('register', 'AuthController.register');
 	Route.post('login', 'AuthController.login');
-
+	// ======================== BLOGS ======================= //
 	Route.group(() => {
 		Route.get('/', 'BlogController.list');
 		Route.get('/:id', 'BlogController.get');
-		Route.post('/new', 'BlogController.store');
-		Route.patch('/:id', 'BlogController.edit');
-		Route.delete('/:id', 'BlogController.delete');
-	})
-		.prefix('blogs')
-		.middleware('auth:api');
+		Route.group(() => {
+			Route.post('/new', 'BlogController.store');
+			Route.patch('/:id', 'BlogController.edit');
+			Route.delete('/:id', 'BlogController.delete');
+		}).middleware('auth:api');
+	}).prefix('blogs');
+	// ======================== POSTS ======================= //
+	Route.group(() => {
+		Route.get('/', 'PostController.list');
+		Route.get('/:id', 'PostController.get');
+		Route.group(() => {
+			Route.post('/new', 'PostController.store');
+			Route.patch('/:id', 'PostController.edit');
+			Route.delete('/:id', 'PostController.delete');
+		}).middleware('auth:api');
+	}).prefix('posts');
 }).prefix('api');
