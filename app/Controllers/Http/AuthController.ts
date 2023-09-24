60abd6755c43b99b29f7f94870aa3262398b23c1
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2023-09-24 20:47:41 +0200

    Improved for user persistance

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,6 +1,14 @@
+import Blog from 'App/Models/Blog';
 import User from 'App/Models/User';
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 export default class AuthController {
+	public async get({ request }) {
+		const user = await User.findBy('remember_me_token', request.body().token);
+		const blog = await Blog.findBy('user_id', user?.id);
+
+		return { user, blog };
+	}
+
 	public async login({ request, response, auth }: HttpContextContract) {
 		const email = request.input('email');
 		const password = request.input('password');
@@ -8,10 +16,17 @@ export default class AuthController {
 		const token = await auth.use('api').attempt(email, password, {
 			expiresIn: '10 days',
 		});
-
 		if (token) {
 			const user = await User.findBy('email', email);
-			return { token: token.token, email: email, alias: user?.alias };
+			user.rememberMeToken = token.token;
+			await user.save();
+			const blog = await Blog.findBy('user_id', user?.id);
+			return {
+				token: token.token,
+				email: email,
+				alias: user?.alias,
+				blog,
+			};
 		} else {
 			return response.badRequest();
 		}
